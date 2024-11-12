c47554493c8701c1bd8999bda73085701bcec002
NguyenMinhTuBH00550 <153350949+NguyenMinhTuBH00550@users.noreply.github.com>
2024-11-12 13:24:30 +0700

    CheckOut nhe tam

---
+++ EcommerceChatbot/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class OrderController : Controller
+{
+    public IActionResult RedirectToVNPay(decimal totalAmount)
+    {
+        // Tạo link thanh toán VNPay dựa trên các thông số yêu cầu của VNPay
+        var vnp_Url = "  http://sandbox.vnpayment.vn/tryitnow/Home/CreateOrder";
+
+        // Các tham số cần thiết cho VNPay
+        var paymentUrl = $"{vnp_Url}?amount={totalAmount * 100}"; // số tiền tính theo đồng, chuyển đổi sang đơn vị nhỏ nhất nếu cần
+
+        // Chuyển hướng người dùng đến URL thanh toán của VNPay
+        return Redirect(paymentUrl);
+    }
+}
