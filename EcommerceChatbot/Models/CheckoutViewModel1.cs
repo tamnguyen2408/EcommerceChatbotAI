fec9092357d827ce57d6fdbf7cfb0c9b47045cda
NguyenMinhTuBH00550 <153350949+NguyenMinhTuBH00550@users.noreply.github.com>
2024-11-26 15:40:50 +0700

    updatebug

---
+++ EcommerceChatbot/Models/CheckoutViewModel1.cs
@@ -0,0 +1,14 @@
+﻿using EcommerceChatbot.Controllers;
+
+namespace EcommerceChatbot.Models
+{
+    public class CheckoutViewModel1
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<CartItem> CartItems { get; set; }
+    }
+}
