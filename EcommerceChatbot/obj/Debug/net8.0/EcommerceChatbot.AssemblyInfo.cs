b41876926a77d49e6fafd89177183b25df08de96
tamnguyen2408 <nguyenngoctam082004@gmail.com>
2024-12-10 20:39:14 +0700

    editMain2

--- EcommerceChatbot/obj/Debug/net8.0/EcommerceChatbot.AssemblyInfo.cs
+++ EcommerceChatbot/obj/Debug/net8.0/EcommerceChatbot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf9cf2d31478a744bf8abce2217aa02903478803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f91ee463c42e5cbbbe98b3973a2c6b13fdc6393")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
